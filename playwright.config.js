06edac0acb8f8f75a0e4680c0ad0813694d69320
arpana12345 <arpanaadarshi003@gmail.com>
2026-04-19 16:19:06 +0530

    Run tests on Chrome + Firefox + WebKit in parallel

--- playwright.config.js
+++ playwright.config.js
@@ -35,5 +35,13 @@ module.exports = defineConfig({
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
   ],
 });
